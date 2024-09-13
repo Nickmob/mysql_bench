1138cf3d43fa69130ea150d6e4bd6cd9fb52e7fc
Nick Lavlinsky <nick@lavlinsky.ru>
2024-09-13 18:08:24 +0300

    Подчистка БД, исправление переменных

--- sysbench.sh
+++ sysbench.sh
@@ -45,23 +45,32 @@ sysbench ./oltp_point_select.lua --threads=$threads_num --mysql-host=$mysql_host
 mysql -u$mysql_user -p$mysql_pass -e 'PURGE BINARY LOGS BEFORE NOW();'
 sysbench ./oltp_point_select.lua --threads=$threads_num --events=0 --time=$bench_time --mysql-host=$mysql_host --mysql-user=$mysql_user --mysql-password=$mysql_pass --mysql-port=$mysql_port --tables=$table_num --table-size=$table_size --range_selects=off --db-ps-mode=disable --report-interval=0 run
 
+mysqladmin -u$mysql_user -p$mysql_pass -f drop $db_sysbench
+mysqladmin -u$mysql_user -p$mysql_pass create $db_sysbench
+
 # RO traffic
 sysbench ./oltp_read_only.lua --threads=$threads_num --mysql-host=$mysql_host --mysql-user=$mysql_user --mysql-password=$mysql_pass --mysql-port=$mysql_port --tables=$table_num --table-size=$table_size prepare
+mysql -u$mysql_user -p$mysql_pass -e 'PURGE BINARY LOGS BEFORE NOW();'
 sysbench ./oltp_read_only.lua --threads=$threads_num --events=0 --time=$bench_time --mysql-host=$mysql_host --mysql-user=$mysql_user --mysql-password=$mysql_pass --mysql-port=$mysql_port --tables=$table_num --table-size=$table_size --range_selects=off --db-ps-mode=disable --report-interval=0 run
 sysbench ./oltp_read_only.lua --threads=$threads_num --events=0 --time=$bench_time --mysql-host=$mysql_host --mysql-user=$mysql_user --mysql-password=$mysql_pass --mysql-port=$mysql_port --tables=$table_num --table-size=$table_size --range_selects=off --db-ps-mode=disable --report-interval=0 cleanup
-mysql -u$mysql_user -p$mysql_pass -e 'PURGE BINARY LOGS BEFORE NOW();'
+
+
+mysqladmin -u$mysql_user -p$mysql_pass -f drop $db_sysbench
+mysqladmin -u$mysql_user -p$mysql_pass create $db_sysbench
 
 # RW traffic
 sysbench ./oltp_read_write.lua --threads=$threads_num --mysql-host=$mysql_host --mysql-user=$mysql_user --mysql-password=$mysql_pass --mysql-port=$mysql_port --tables=$table_num --table-size=$table_size0 prepare
+mysql -u$mysql_user -p$mysql_pass -e 'PURGE BINARY LOGS BEFORE NOW();'
 sysbench ./oltp_read_write.lua --threads=$threads_num --events=0 --time=$bench_time --mysql-host=$mysql_host --mysql-user=$mysql_user --mysql-password=$mysql_pass --mysql-port=$mysql_port --tables=$table_num --delete_inserts=10 --index_updates=10 --non_index_updates=10 --table-size=$table_size0 --db-ps-mode=disable --report-interval=0 run
 sysbench ./oltp_read_write.lua --threads=$threads_num --events=0 --time=$bench_time --mysql-host=$mysql_host --mysql-user=$mysql_user --mysql-password=$mysql_pass --mysql-port=$mysql_port --tables=$table_num --delete_inserts=10 --index_updates=10 --non_index_updates=10 --table-size=$table_size0 --db-ps-mode=disable --report-interval=0 cleanup
 
 mysql -u$mysql_user -p$mysql_pass -e 'PURGE BINARY LOGS BEFORE NOW();'
-mysqladmin -u$mysql_user -p$mysql_pass drop $db_sysbench
+mysqladmin -u$mysql_user -p$mysql_pass -f drop $db_sysbench
+
 # TPCC Percona
 cd /usr/share/sysbench/percona
 
-mysqladmin -u$mysql_user -p$mysql_pass -f drop sbt
+mysqladmin -u$mysql_user -p$mysql_pass -f drop $db_tpcc
 mysqladmin -u$mysql_user -p$mysql_pass create $db_tpcc
 mysql -u$mysql_user -p$mysql_pass -e 'PURGE BINARY LOGS BEFORE NOW();'
 
@@ -70,4 +79,4 @@ mysql -u$mysql_user -p$mysql_pass -e 'PURGE BINARY LOGS BEFORE NOW();'
 
 ./tpcc.lua --mysql-user=$mysql_user --mysql-password=$mysql_pass --mysql-host=$mysql_host --mysql-port=$mysql_port --mysql-db=$db_tpcc --time=$bench_time --threads=$threads_num --report-interval=0 --tables=$table_num --scale=2 --db-driver=mysql run
 
-mysqladmin -u$mysql_user -p$mysql_pass -f drop sbt
+mysqladmin -u$mysql_user -p$mysql_pass -f $db_tpcc
