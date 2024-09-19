f5fc66a8b6029a12c5ea740bc373af73d91fbe7f
Nick Lavlinsky <nick@lavlinsky.ru>
2024-09-19 12:46:15 +0300

    Увеличен размер данных для тестов до 1GB

--- sysbench.sh
+++ sysbench.sh
@@ -15,9 +15,9 @@ mysql_port=3306
 # Количество потоков
 threads_num=4
 # Время теста
-bench_time=10
+bench_time=30
 # Размер таблицы (sysbench)
-table_size=100000
+table_size=2000000
 # Количество таблиц (sysbench)
 table_num=2
 # Опции sysbench
@@ -67,11 +67,12 @@ mysqladmin -u$mysql_user -p$mysql_pass -f drop $db_tpcc > /dev/null
 mysqladmin -u$mysql_user -p$mysql_pass create $db_tpcc > /dev/null
 mysql -u$mysql_user -p$mysql_pass -e 'PURGE BINARY LOGS BEFORE NOW();' > /dev/null
 
-./tpcc.lua --mysql-user=$mysql_user --mysql-password=$mysql_pass --mysql-host=$mysql_host --mysql-port=$mysql_port --mysql-db=$db_tpcc --threads=$threads_num --tables=$table_num --scale=2 prepare > /dev/null
+# Scale - количество warehouse. scale 100 + table 1 = 10 GB данных
+./tpcc.lua --mysql-user=$mysql_user --mysql-password=$mysql_pass --mysql-host=$mysql_host --mysql-port=$mysql_port --mysql-db=$db_tpcc --threads=$threads_num --tables=$table_num --scale=5 prepare > /dev/null
 mysql -u$mysql_user -p$mysql_pass -e 'PURGE BINARY LOGS BEFORE NOW();' > /dev/null
 
 echo '>>>>>>>>>>> Test: tpcc.lua <<<<<<<<<<<<'
-./tpcc.lua --mysql-user=$mysql_user --mysql-password=$mysql_pass --mysql-host=$mysql_host --mysql-port=$mysql_port --mysql-db=$db_tpcc --time=$bench_time --threads=$threads_num --report-interval=0 --tables=$table_num --scale=2 --db-driver=mysql $sysb_opts run
+./tpcc.lua --mysql-user=$mysql_user --mysql-password=$mysql_pass --mysql-host=$mysql_host --mysql-port=$mysql_port --mysql-db=$db_tpcc --time=$bench_time --threads=$threads_num --report-interval=0 --tables=$table_num --scale=5 --db-driver=mysql $sysb_opts run
 
 mysqladmin -u$mysql_user -p$mysql_pass -f drop $db_tpcc > /dev/null
 echo 'Done!'
